d34bb05c00d2fa7a32ed060e461d92839326a53f
Azizbek Qozoqov <73469043+azizbekQozoqov@users.noreply.github.com>
2025-10-21 14:53:29 +0500

    done first

--- core/settings.py
+++ core/settings.py
@@ -136,6 +136,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+# Where collectstatic will put files for production serving
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
