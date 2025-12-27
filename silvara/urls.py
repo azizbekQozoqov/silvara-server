0e28baeb2f104df9dd78156e61e02b9d85d376ed
Azizbek Qozoqov <73469043+azizbekQozoqov@users.noreply.github.com>
2025-12-28 03:24:13 +0500

    Project cleared

--- silvara/urls.py
+++
@@ -1,9 +0,0 @@
-from .views import index, webhook, raviosta_bot, raviosta_order_api
-from django.urls import path
-
-urlpatterns = [
-    path('', index),
-    path('webhook/', webhook, name='telegram_webhook'),
-    path('raviosta/bot/', raviosta_bot, name='raviosta_bot'),
-    path('raviosta/order', raviosta_order_api, name='raviosta_order_api'),
-]
